17f186d02670ce0c7392fc69f98f3131b91fb632
Tuuli Kauri <170147490+tuulikauri@users.noreply.github.com>
2026-01-20 17:11:30 -0500

    Incorporating PR14164 review comment from 2025-04-27

--- src/soundio/soundmanager.cpp
+++ src/soundio/soundmanager.cpp
@@ -540,7 +540,7 @@ SoundDeviceStatus SoundManager::setupDevices() {
     // Get all outputs for each device
     QHash<SoundDevicePointer, QList<AudioOutput>> deviceOutputs;
     for (const auto& pDevice : std::as_const(m_devices)) {
-        deviceOutputs[pDevice] = m_config.getOutputs().values(pDevice->getDeviceId());
+        deviceOutputs.insert(pDevice, m_config.getOutputs().values(pDevice->getDeviceId()));
         // Statically connect the Network Device to the Sidechain
         if (pDevice->getDeviceId().name == kNetworkDeviceInternalName) {
             AudioOutput out(AudioPathType::RecordBroadcast,
